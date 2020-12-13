502c29a6cdf8d0a4a05fc965e5298e6ebc1ce41f
gabrieldocs <mydocs.lucas@gmail.com>
2020-12-13 15:20:31 -0300

    iniciando implementação parte 4

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Question
-
+from django.urls import reverse
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]        
@@ -21,4 +21,16 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse('You are vonting on %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) 
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html', {
+            'question':question,
+            'error_message': "You didn't selected a choice"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
