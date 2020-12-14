a61d13de6c123bb274cb2bf5b12bd34c8483baa1
gabrieldocs <mydocs.lucas@gmail.com>
2020-12-14 10:50:34 -0300

    configurando aplicação com static files - ok

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,21 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice 
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = (
+        ('Question Content', { 'fields' : ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+        )
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
