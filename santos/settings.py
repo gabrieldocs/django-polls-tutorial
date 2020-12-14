4cab674333eba82f8f15b4cf7bd3aa26dd8d4b06
gabrieldocs <mydocs.lucas@gmail.com>
2020-12-14 14:21:23 -0300

    adicionando django-jet

--- santos/settings.py
+++ santos/settings.py
@@ -27,10 +27,53 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#jet themes 
+
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
+
+JET_SIDE_MENU_COMPACT = False
+JET_DEFAULT_THEME = 'light-green'
+JET_CHANGE_FORM_SIBLING_LINKS = True
+JET_APP_INDEX_DASHBOARD = 'jet.dashboard.dashboard.DefaultAppIndexDashboard'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- santos/urls.py
+++ santos/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls)
 ]
